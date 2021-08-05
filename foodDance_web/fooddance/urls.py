c9061895cd15b4b1bce46f9ee49484f520a157d8
Zumin Li <2278100l@student.gla.ac.uk>
2021-08-05 10:50:22 +0100

    Detail page:add view,static files and template

--- foodDance_web/fooddance/urls.py
+++ foodDance_web/fooddance/urls.py
@@ -3,6 +3,8 @@ from django.urls import path
 from django.urls import include
 from . import views
 
+app_name = 'fooddance'
+
 urlpatterns = [
     path('', views.index, name='index'),
     path('<slug:recipe_title_slug>', views.detail, name="detail")
--- foodDance_web/fooddance/views.py
+++ foodDance_web/fooddance/views.py
@@ -1,13 +1,41 @@
 from django.shortcuts import render
-from .models import Recipe
-
+from .models import Recipe, UserProfile, Comment, RecipeStep
+from django.db.models import Avg
+from .forms import *
+from django.shortcuts import redirect
 # Create your views here.
+
+
+def dict2obj(args):
+    class obj(object):
+        def __init__(self, d):
+            for a, b in d.items():
+                if isinstance(b, (list, tuple)):
+                    setattr(self, a, [obj(x) if isinstance(x, dict) else x for x in b])
+                else:
+                    setattr(self, a, obj(b) if isinstance(b, dict) else b)
+    return obj(args)
 
 
 def index(request):
 
-    context_dict = {'recipes': Recipe.objects.all()}
+    recipes = []
 
+    def takeAverage(ele):
+        return ele.avg
+
+    for recipe in Recipe.objects.all():
+        recipes.append(
+            dict2obj({
+                "title": recipe.title,
+                "avg": Comment.objects.filter(recipe__id=recipe.id)
+                    .aggregate(Avg('rating'))
+            })
+        )
+
+    recipes.sort(key=takeAverage)
+
+    context_dict = {'recipes': recipes}
     return render(request, 'fooddance/index.html', context_dict)
 
 
@@ -17,7 +45,26 @@ def detail(request, recipe_title_slug):
     try:
         recipe = Recipe.objects.get(slug=recipe_title_slug)
         context_dict['recipe'] = recipe
+        context_dict['author_profile'] = UserProfile.objects.get(user_id=recipe.author.id)
+        context_dict['avg'] = int(Comment.objects.filter(recipe__id=recipe.id).aggregate(Avg('rating'))['rating__avg'])
+        context_dict['collect'] = UserProfile.objects.filter(collections__slug=recipe.slug).count()
+        context_dict['steps'] = RecipeStep.objects.filter(recipe_id=recipe.id)
+        context_dict['comments'] = Comment.objects.filter(recipe__id=recipe.id)
+
     except Recipe.DoesNotExist:
         context_dict['recipe'] = None
+
+    form = CommentForm()
+
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+
+        if form.is_valid():
+            form.save(commit=True)
+            return redirect('/index/')
+        else:
+            print(form.errors)
+
+    context_dict['form'] = form
 
     return render(request, 'fooddance/detail.html', context=context_dict)
